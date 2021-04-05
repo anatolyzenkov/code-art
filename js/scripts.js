47def8db699ad35a5732a034898e88d094085a68
Anatoly Zenkov <me@anatolyzenkov.com>
2021-04-06 00:06:27 +0200

    Line with line intersection formula added

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-const PI2 = Math.PI;
+// UTILS
 
-const deltaAngle = (a0, a1) => {
-    const da = (a1 - a0) % PI2;
-    return ((2 * da) % PI2) - da;
+const getDevicePixelRatio = () => {
+    if (window.devicePixelRatio) return window.devicePixelRatio;
+    return 1;
 };
 
 const isLocalHost = () => location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.origin === 'file://';
 
 const isMobileDevice = () => {
-  try{ document.createEvent("TouchEvent"); return true; }
-  catch(e){ return false; }
+    try{ document.createEvent("TouchEvent"); return true; }
+    catch(e){ return false; }
 }
 
 // Posted by Andy Langton on Tuesday, 4 December 2007
@@ -35,31 +35,63 @@ const getViewport = () => {
     return [viewPortWidth, viewPortHeight];
 };
 
-const pointPolygonHitTest = (p, poly) => {
-    // ray-casting algorithm based on
-    // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
-    let inside = false;
-    if (poly[0].x !== undefined) {
-        for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
-            let xi = poly[i].x, yi = poly[i].y;
-            let xj = poly[j].x, yj = poly[j].y;
-            let intersect = yi > p.y != yj > p.y && p.x < ((xj - xi) * (p.y - yi)) / (yj - yi) + xi;
-            if (intersect) inside = !inside;
-        }
-    } else {
-        for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
-            let xi = poly[i][0], yi = poly[i][1];
-            let xj = poly[j][0], yj = poly[j][1];
-            let intersect = yi > p.y != yj > p.y && p.x < ((xj - xi) * (p.y - yi)) / (yj - yi) + xi;
-            if (intersect) inside = !inside;
-        }
+// MATH and GEOM
+
+const PI2 = Math.PI;
+
+class Point {
+    x; y;
+    constructor(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
     }
-    return inside;
+}
+
+const deltaAngle = (a0, a1) => {
+    const da = (a1 - a0) % PI2;
+    return ((2 * da) % PI2) - da;
 };
 
-const getDevicePixelRatio = () => {
-  if (window.devicePixelRatio) return window.devicePixelRatio;
-  return 1;
+const lineLineIntersection = (p0, p1, p2, p3) => {
+    // Line p0p1 represented as a1x + b1y = c1
+    const a1 = p1.y - p0.y;
+    const b1 = p0.x - p1.x;
+    const c1 = a1*p0.x + b1*p0.y;
+    // Line p2p3 represented as a2x + b2y = c2
+    const a2 = p3.y - p2.y;
+    const b2 = p2.x - p3.x;
+    const c2 = a2*p2.x+ b2*p2.y;
+    const determinant = a1*b2 - a2*b1;
+    if (determinant === 0) return; // The lines are parallel
+    const x = (b2*c1 - b1*c2)/determinant;
+    const y = (a1*c2 - a2*c1)/determinant;
+    return {x: x, y: y};
+};
+
+const pointPointDistance = (p0, p1) => {
+    return Math.sqrt((p0.x-p1.x)*(p0.x-p1.x) + (p0.y-p1.y)*(p0.y-p1.y));
+}
+
+const pointPolygonHitTest = (p, poly) => {
+  // ray-casting algorithm based on
+  // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
+  let inside = false;
+  if (poly[0].x !== undefined) {
+      for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+          let xi = poly[i].x, yi = poly[i].y;
+          let xj = poly[j].x, yj = poly[j].y;
+          let intersect = yi > p.y != yj > p.y && p.x < ((xj - xi) * (p.y - yi)) / (yj - yi) + xi;
+          if (intersect) inside = !inside;
+      }
+  } else {
+      for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+          let xi = poly[i][0], yi = poly[i][1];
+          let xj = poly[j][0], yj = poly[j][1];
+          let intersect = yi > p.y != yj > p.y && p.x < ((xj - xi) * (p.y - yi)) / (yj - yi) + xi;
+          if (intersect) inside = !inside;
+      }
+  }
+  return inside;
 };
 
 const clamp = (n, min, max) => {
@@ -67,53 +99,53 @@ const clamp = (n, min, max) => {
 };
 
 class MotionSimulator {
-  x = 0;
-  y = 0;
-  _m = 0.3;
-  _automatic = true;
-  _a0 = Math.random() * PI2;
-  _a1 = Math.random() * PI2;
-  _a2 = Math.random() * PI2;
-  _a3 = Math.random() * PI2;
-  _a4 = Math.random() * PI2;
-  _a5 = Math.random() * PI2;
-  constructor() {
-    document.addEventListener('touchmove', (e) => {
-      const touch = e.touches[0] || e.changedTouches[0];
-      this.x = touch.pageX;
-      this.y = touch.pageY;
-      e.preventDefault();
-      manualSwitcher();
-    });
-    document.addEventListener('mousemove', (e) => {
-      this.x = e.pageX;
-      this.y = e.pageY;
-      manualSwitcher();
-    });
-    let timeoutID = -1;
-    const manualSwitcher = () => {
-      this._automatic = false;
-      clearTimeout(timeoutID);
-      timeoutID = setTimeout(() => {
-        this._automatic = true;
-      }, 5000);
+    x = 0;
+    y = 0;
+    _m = 0.3;
+    _automatic = true;
+    _a0 = Math.random() * PI2;
+    _a1 = Math.random() * PI2;
+    _a2 = Math.random() * PI2;
+    _a3 = Math.random() * PI2;
+    _a4 = Math.random() * PI2;
+    _a5 = Math.random() * PI2;
+    constructor() {
+        document.addEventListener('touchmove', (e) => {
+            const touch = e.touches[0] || e.changedTouches[0];
+            this.x = touch.pageX;
+            this.y = touch.pageY;
+            e.preventDefault();
+            manualSwitcher();
+        });
+        document.addEventListener('mousemove', (e) => {
+            this.x = e.pageX;
+            this.y = e.pageY;
+            manualSwitcher();
+        });
+        let timeoutID = -1;
+        const manualSwitcher = () => {
+            this._automatic = false;
+            clearTimeout(timeoutID);
+            timeoutID = setTimeout(() => {
+                this._automatic = true;
+            }, 5000);
+        }
+        this.update = () => {
+            if (this._automatic) {
+                const w = getViewport()[0];
+                const h = getViewport()[1];
+                this._a1 += this._m * 0.012;
+                this._a2 += this._m * 0.02;
+                this._a3 += this._m * 0.03;
+                this._a4 += this._m * 0.04;
+                this._a5 += this._m * 0.1;
+                this._a0 += this._m * 0.07 * Math.cos(this._a2) * Math.sin(this._a3);
+                let r = Math.max(w, h)/4;
+                r = r + r * 2 / 3 * Math.cos(this._a1) * Math.sin(this._a4) - r * 2 / 3 * Math.sin(this._a5);
+                this.x = w/2 + r * Math.cos(this._a0);
+                this.y = h/2 + r * Math.sin(this._a0);
+            }
+        }
     }
-    this.update = () => {
-      if (this._automatic) {
-        const w = getViewport()[0];
-        const h = getViewport()[1];
-        this._a1 += this._m * 0.012;
-        this._a2 += this._m * 0.02;
-        this._a3 += this._m * 0.03;
-        this._a4 += this._m * 0.04;
-        this._a5 += this._m * 0.1;
-        this._a0 += this._m * 0.07 * Math.cos(this._a2) * Math.sin(this._a3);
-        let r = Math.max(w, h)/4;
-        r = r + r * 2 / 3 * Math.cos(this._a1) * Math.sin(this._a4) - r * 2 / 3 * Math.sin(this._a5);
-        this.x = w/2 + r * Math.cos(this._a0);
-        this.y = h/2 + r * Math.sin(this._a0);
-      }
-    }
-  }
 }
 ;
